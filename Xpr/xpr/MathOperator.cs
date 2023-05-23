ac960db473b145e824c2cfa5ed0406454eb1f2fb
timur.kozlov <timur.kozlov@gmail.com>
2023-05-23 17:30:56 +0300

    MathOperatorEx

--- Xpr/xpr/MathOperator.cs
+++ Xpr/xpr/MathOperator.cs
@@ -9,3 +9,28 @@ public enum MathOperator
     Power,
     Undefined
 }
+
+public static class MathOperatorEx
+{
+    public static readonly char[] Chars = {'+', '-', '*', '/', '^'};
+    
+    public static char getChar(this MathOperator val)
+    {
+        return Chars[(int)val];
+    }
+
+    public static bool resolve(char next, out MathOperator op)
+    {
+        var index = Array.IndexOf(Chars, next);
+        if (index != -1)
+        {
+            op = (MathOperator)index;
+        }
+        else
+        {
+            op = MathOperator.Undefined;
+        }
+
+        return index != -1;
+    }
+}
