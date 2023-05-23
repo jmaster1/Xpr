b7e5592c1bd83ec8129312c3d88650647af7b192
timur.kozlov <timur.kozlov@gmail.com>
2023-05-23 14:40:36 +0300

    test

---
+++ XprTest/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace XprTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            float result = new Xpr.xpr.Xpr("1+1").eval();
+            Assert.True(true);
+        }
+    }
+}
