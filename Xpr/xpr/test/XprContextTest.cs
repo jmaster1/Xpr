f2b1c2ef3f2b4e36da1b1b03252b8d283e30206f
timur <timur.kozlov@gmail.com>
2023-05-26 14:11:15 +0300

    TestFunc2, TestFuncN

--- Xpr/xpr/test/XprContextTest.cs
+++ Xpr/xpr/test/XprContextTest.cs
@@ -22,4 +22,24 @@ public class XprContextTest : XprTest
             ctx.Funcs1["eq"] = arg1 => arg1;
         });
     }
+    
+    [Test]
+    public void TestFunc2()
+    {
+    
+        EvalEq(3, "second(1, 3)", ctx =>
+        {
+            ctx.Funcs2["second"] = (arg1, arg2) => arg2;
+        });
+    }
+    
+    [Test]
+    public void TestFuncN()
+    {
+    
+        EvalEq(3, "last(1, 2, 3)", ctx =>
+        {
+            ctx.FuncsN["last"] = args => args[^1];
+        });
+    }
 }
